84bfc919493519109db9099f8d1a3582d383d764
Clotia Johari <freewillandjustice@gmail.com>
2022-01-05 18:42:26 -0500

    removed extraneous file

--- utils/users.js
+++ utils/users.js
@@ -9,28 +9,28 @@ function userJoin(id, username, room) {
   return user;
 }
 
-// Get current user
+// Fetch current user
 function getCurrentUser(id) {
-  return users.find(user => user.id === id);
+  return users.find((user) => user.id === id);
 }
 
 // User leaves chat
 function userLeave(id) {
-  const index = users.findIndex(user => user.id === id);
+  const index = users.findIndex((user) => user.id === id);
 
   if (index !== -1) {
     return users.splice(index, 1)[0];
   }
 }
 
-// Get room users
+// Fetch room users
 function getRoomUsers(room) {
-  return users.filter(user => user.room === room);
+  return users.filter((user) => user.room === room);
 }
 
 module.exports = {
   userJoin,
   getCurrentUser,
   userLeave,
-  getRoomUsers
+  getRoomUsers,
 };
